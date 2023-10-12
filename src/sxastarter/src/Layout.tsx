c741d9dcf439df978411b8f08e2b771e8b566ae5
ichalvatzis <70324268+ichalvatzis@users.noreply.github.com>
2023-10-12 20:51:39 +0300

    Add files via upload

--- src/sxastarter/src/Layout.tsx
+++ src/sxastarter/src/Layout.tsx
@@ -31,13 +31,13 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
     <div>
       <Scripts />
       <Head>
-        <title>{fields?.Title?.value?.toString() || 'Page'}</title>
-        <link rel="icon" href={`${publicUrl}/favicon.ico`} />
-        {headLinks.map((headLink) => (
-          <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
-        ))}
-        <script type="text/javascript" src="/config.js" />
-        <script type="text/javascript" src="https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.8.min.js" />
+		<title>{fields?.Title?.value?.toString() || 'Page'}</title>
+		  <link rel="icon" href={`${publicUrl}/favicon.ico`} />
+		  {headLinks.map((headLink) => (
+			<link rel={headLink.rel} key={headLink.href} href={headLink.href} />
+		  )}
+		  <script type="text/javascript" src="/config.js" async />
+		  <script type="text/javascript" src="https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.8.min.js" async />
       </Head>
 
       {/* root placeholder for the app, which we add components to using route data */}
