e4374ab5655a799a55f729ca9e7d84c3430b0f0e
ichalvatzis <70324268+ichalvatzis@users.noreply.github.com>
2023-10-12 22:45:09 +0300

    Update config.js

--- src/sxastarter/public/config.js
+++ src/sxastarter/public/config.js
@@ -1,15 +1,21 @@
-<script type = "text/javascript"> 
-    var _boxeverq = _boxeverq || [],
-        _boxever_settings = {
-            client_key: "33b84227fa50a93c827d9adbc3e73e79",
-            target: "https://api-engage-eu.sitecorecloud.io",
-            cookie_domain: "personalize-sandbox-basic.vercel.app",
-            pointOfSale: "Default",
-            web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net"
-        };
-! function() {
-    var e = document.createElement("script");
-    e.type = "text/javascript", e.async = !0, e.src = "https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.8.min.js";
-    var t = document.getElementsByTagName("script")[0];
-    t.parentNode.insertBefore(e, t)
-}() </script> 
+// Define the Boxever queue
+var _boxeverq = _boxeverq || [];
+
+// Define the Boxever settings
+var _boxever_settings = {
+    client_key: '33b84227fa50a93c827d9adbc3e73e79', // Replace with your client key
+    target: 'https://api-engage-eu.sitecorecloud.io', // Replace with your API target endpoint specific to your data center region
+    cookie_domain: 'personalize-sandbox-basic.vercel.app', // Replace with the top-level cookie domain of the website
+    web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net",
+    pointOfSale: "Default" // Replace with the point of sale you have configured in your tenant
+};
+
+// Import the Boxever library asynchronously
+(function() {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = 'https://d1mj578wat5n4o.cloudfront.net/boxever-{{clientVersion}}.min.js';
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+})();
