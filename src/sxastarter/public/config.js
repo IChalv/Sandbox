c9fdf961c133b0554e9ca3371b0d29c24ee44a21
ichalvatzis <70324268+ichalvatzis@users.noreply.github.com>
2023-10-13 01:20:03 +0300

    Update config.js

--- src/sxastarter/public/config.js
+++ src/sxastarter/public/config.js
@@ -4,7 +4,7 @@ var _boxeverq = _boxeverq || [];
 // Define the Boxever settings
 var _boxever_settings = {
     client_key: '33b84227fa50a93c827d9adbc3e73e79', // Replace with your client key
-    target: 'https://api.boxever.com/v1.2'', // Replace with your API target endpoint specific to your data center region
+    target: 'https://api.boxever.com/v1.2', // Replace with your API target endpoint specific to your data center region
     cookie_domain: '.personalize-sandbox-basic.vercel.app', // Replace with the top-level cookie domain of the website
     web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net",
     pointOfSale: "Default" // Replace with the point of sale you have configured in your tenant
