5cb35c47416b1866c8447132dbe391a0bbcc7726
ichalvatzis <70324268+ichalvatzis@users.noreply.github.com>
2023-10-12 20:34:54 +0300

    Add files via upload

---
+++ src/sxastarter/public/config.js
@@ -0,0 +1,15 @@
+<script type = "text/javascript"> 
+    var _boxeverq = _boxeverq || [],
+        _boxever_settings = {
+            client_key: "33b84227fa50a93c827d9adbc3e73e79",
+            target: "https://api-engage-eu.sitecorecloud.io",
+            cookie_domain: "personalize-sandbox-basic.vercel.app",
+            pointOfSale: "Default",
+            web_flow_target: "https://d35vb5cccm4xzp.cloudfront.net"
+        };
+! function() {
+    var e = document.createElement("script");
+    e.type = "text/javascript", e.async = !0, e.src = "https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.8.min.js";
+    var t = document.getElementsByTagName("script")[0];
+    t.parentNode.insertBefore(e, t)
+}() < /script> 
